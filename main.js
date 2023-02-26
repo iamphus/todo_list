1b5786fe10c0814a4ff4b0a9a6ec03ca54c4341b
iamphus <phamthanhphu35@gmail.com>
2023-02-26 23:48:37 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+let inputText = document.getElementById('input-text');
+let addBtn = document.getElementById('addBtn');
+let todoItemsWrapper = document.getElementById('todo-items-wrapper');
+let itemCount = document.getElementById('totalItem');
+
